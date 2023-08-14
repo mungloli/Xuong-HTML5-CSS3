5bc6ce7d75a4aff6520c324af0e5d0d7256ef9b0
longdv ph29867 <emchuaroinhe@gmail.com>
2023-08-14 22:32:44 +0700

    update memberShip Page

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,6 +1,8 @@
-let header = document.querySelector('header');
-let btn_menu = document.querySelector('.btn-show-menu');
+let header = document.querySelector("header");
+let btn_menu = document.querySelector(".btn-show-menu");
 
 btn_menu.onclick = function () {
-    header.classList.toggle("show-menu");
-}
+  header.classList.toggle("show-menu");
+};
+
+
