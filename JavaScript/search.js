0f22ddce31f9f9db977b7a6445d9fd62c2fda4f0
trieulinh01 <107738933+trieulinh01@users.noreply.github.com>
2023-08-16 21:52:33 +0700

    searchPage

---
+++ JavaScript/search.js
@@ -0,0 +1,11 @@
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("hidden");
+ 
+
+}
+function myFunction1() {
+  document.getElementById("myDropdown1").classList.toggle("hidden");
+}
+
+
+
